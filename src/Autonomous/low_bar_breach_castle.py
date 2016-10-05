f2fd74cfb6d3205db09b40cdeaaf99a5cd19be43
Kevin <Kevin@10.101.62.6>
2016-10-05 19:23:21 -0400

    Created extensions of Low Bar Breach Stop for purpose of castle attack or defense breaching

---
+++ src/Autonomous/low_bar_breach_castle.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 5, 2016
+
+@author: Kevin
+'''
+
+class Low_Bar_Breach_Castle(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ src/Autonomous/low_bar_breach_defense.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 5, 2016
+
+@author: Kevin
+'''
+
+class Low_Bar_Breach_Defense(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
