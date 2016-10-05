c2b24f418fcb9b0ac5deb740a183a5fe79888f76
DrWateryCat <DrWateryCat@gmail.com>
2016-10-05 18:41:55 -0400

    Added a low bar breaching autonomous mode

---
+++ src/Autonomous/low_bar_breach_stop.py
@@ -0,0 +1,49 @@
+'''
+Created on Oct 5, 2016
+
+@author: Kenny
+'''
+
+from robotpy_ext.autonomous import StatefulAutonomous, timed_state
+
+class Low_Bar_Breach_Stop(StatefulAutonomous):
+    '''
+    classdocs
+    '''
+    
+    MODE_NAME = "Low Bar Breach Stop"
+
+    def initialize(self):
+        pass
+    
+    @timed_state(duration=3, first=True, next_state='stop')
+    def go_forward(self):
+        self.left.set(1)
+        self.right.set(1)
+        
+    @timed_state(duration=1, next_state='go_backward')
+    def stop(self):
+        self.left.set(0)
+        self.right.set(0)
+        
+    @timed_state(duration=3, next_state='stop2')
+    def go_backward(self):
+        self.left.set(-1)
+        self.right.set(-1)
+        
+    @timed_state(duration=1, next_state='go_forward2')
+    def stop2(self):
+        self.left.set(0)
+        self.right.set(0)
+    
+    @timed_state(duration=3, next_state='end')
+    def go_forward2(self):
+        self.left.set(1)
+        self.right.set(1)
+        
+    @timed_state(duration=1)
+    def end(self):
+        self.left.set(0)
+        self.right.set(0)
+        
+    
